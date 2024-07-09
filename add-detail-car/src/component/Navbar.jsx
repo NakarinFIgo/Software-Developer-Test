c2430a1dff9889913054c7c3120c7b06cad48980
NakarinFIgo <wangkit.nakarin@gmail.com>
2024-07-09 20:58:49 +0700

    feat: add component

---
+++ add-detail-car/src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div className='shadow-md h-16 py-2 pl-24 text-xl font-bold'>
+            <div className='flex gap-4'>
+                <div className='py-3'>ADD CARs </div>
+                <img className="" width="50" height="50" src="https://img.icons8.com/ios/50/car-top-view.png" alt="car-top-view"/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
