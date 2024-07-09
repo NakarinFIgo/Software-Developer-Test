e4fc56b2ad68b2c1567ba5c1b9cd090675e5ee89
NakarinFIgo <wangkit.nakarin@gmail.com>
2024-07-09 22:39:34 +0700

    feat: add submit form

--- add-detail-car/src/component/Body.jsx
+++ add-detail-car/src/component/Body.jsx
@@ -1,44 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from "react-hook-form";
 
 function Body() {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const [formData,setFormData]= useState({
+        licensePlate:'',
+        brand:'',
+        model:'',
+        note:''
+    });
 
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const onSubmit = data => {
+        const newData = {
+            licensePlate:formData.licensePlate,
+            brand:formData.brand,
+            model:formData.model,
+            note:formData.note
+        }
+        console.log(newData); 
+    }        
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-            <label>ทะเบียนรถ</label>
-            <input 
-                type="text"
-                {...register("licensePlate",{required: true})} 
-            />
-            {errors.licensePlate && <span>จำเป็นต้องกรอกทะเบียนรถ</span>}
-        </div>
-        <div>
-            <label>ยี่ห้อรถ</label>
-            <input 
-                type="text"
-                {...register("brand",{required: true})} 
-            />
-            {errors.brand && <span>จำเป็นต้องกรอกยี่ห้อของรถ</span>}
-        </div>
-        <div>
-            <label>รุ่นรถ</label>
-            <input 
-                type="text"
-                {...register("model",{required: true})} 
-            />
-            {errors.model && <span>จำเป็นต้องกรอกรุ่นของรถ</span>}
-        </div>
-        <div>
-            <label>หมายเหตุุ</label>
-            <input 
-                type="text"
-                {...register("note")} 
-            />
-        </div>
-    </form>
+    <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <input
+                    name='licensePlate'
+                    placeholder='ทะเบียนรถ' 
+                    type="text"
+                    {...register("licensePlate",{required: true})}
+                    value={formData.licensePlate}
+                    onChange={handleChange}  
+                />
+                <div>
+                    {errors.licensePlate && <span>จำเป็นต้องกรอกทะเบียนรถ</span>}
+                </div>
+            </div>
+            <div>
+                <input
+                    name='brand'
+                    placeholder='ยี่ห้อรถ' 
+                    type="text"
+                    {...register("brand",{required: true})}
+                    value={formData.brand}
+                    onChange={handleChange}  
+                />
+                <div>
+                    {errors.brand && <span>จำเป็นต้องกรอกยี่ห้อของรถ</span>}
+                </div>
+            </div>
+            <div>
+                <input
+                    name='model'
+                    placeholder='รุ่นของรถ' 
+                    type="text"
+                    {...register("model",{required: true})}
+                    value={formData.model}
+                    onChange={handleChange}  
+                />
+                <div>
+                    {errors.model && <span>จำเป็นต้องกรอกรุ่นของรถ</span>}
+                </div>
+            </div>
+            <div>
+                <input
+                    name='note'
+                    placeholder='หมายเหตุ' 
+                    type="text"
+                    {...register("note")}
+                    value={formData.note}
+                    onChange={handleChange} 
+                />
+            </div>
+            <button type='submit'>ยืนยัน</button>
+        </form>
+    </div>    
   )
 }
 
