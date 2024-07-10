331428dd4c723564fbab9716a276fef20ee6b979
NakarinFIgo <wangkit.nakarin@gmail.com>
2024-07-10 23:33:14 +0700

    feat: add css

--- add-detail-car/src/component/Body.jsx
+++ add-detail-car/src/component/Body.jsx
@@ -33,6 +33,7 @@ function Body() {
             const response = await axios.put(`${BASE_URL}/cars/${id}`,formData)
             fetchData()
             setEditCar(null)
+            alert('Update success!!')
         } catch (error) {
             console.error('Error update car',error);
         }
@@ -59,6 +60,7 @@ function Body() {
         try {
             await axios.delete(`${BASE_URL}/cars/${id}`)
             fetchData()
+            alert('Delete success!!')
         } catch (error) {
             console.error('Error delete car',error);
         }
@@ -76,6 +78,7 @@ function Body() {
                 note:formData.note
             })
             console.log(response.data);
+            alert('Add success!!')
             fetchData() 
             
         } catch (error) {
@@ -95,77 +98,92 @@ function Body() {
     }
     
   return (
-    <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <input
-                    name='licensePlate'
-                    placeholder='ทะเบียนรถ' 
-                    type="text"
-                    {...register("licensePlate",{required: true})}
-                    value={formData.licensePlate}
-                    onChange={handleChange}  
-                />
-                <div>
-                    {errors.licensePlate && <span>จำเป็นต้องกรอกทะเบียนรถ</span>}
-                </div>
+    <div className='flex gap-20'>
+        <div className='mb-2 ml-20 mt-20' >
+            <div className='flex justify-center mb-4 text-2xl'>
+                <div>ADD NEW CAR</div>
             </div>
-            <div>
-                <input
-                    name='brand'
-                    placeholder='ยี่ห้อรถ' 
-                    type="text"
-                    {...register("brand",{required: true})}
-                    value={formData.brand}
-                    onChange={handleChange}  
-                />
-                <div>
-                    {errors.brand && <span>จำเป็นต้องกรอกยี่ห้อของรถ</span>}
-                </div>
-            </div>
-            <div>
-                <input
-                    name='model'
-                    placeholder='รุ่นของรถ' 
-                    type="text"
-                    {...register("model",{required: true})}
-                    value={formData.model}
-                    onChange={handleChange}  
-                />
-                <div>
-                    {errors.model && <span>จำเป็นต้องกรอกรุ่นของรถ</span>}
-                </div>
-            </div>
-            <div>
-                <input
-                    name='note'
-                    placeholder='หมายเหตุ' 
-                    type="text"
-                    {...register("note")}
-                    value={formData.note}
-                    onChange={handleChange} 
-                />
-            </div>
-            <button type='submit'>{editCar ? 'Update' : 'ยืนยัน'}</button>
-            {editCar && <button type="button" onClick={resetForm}>Cancel</button>}
-        </form>
-        <div>
-            <button onClick={fetchData}>{showData ? 'Hide all cars' : 'Show all cars'}</button>
-            {showData &&
-            carsList.map((val,key)=>{
-                return (
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='mb-2'>
+                    <input
+                        className="border-2 rounded-md p-2 w-[200px]"
+                        name='licensePlate'
+                        placeholder='ทะเบียนรถ' 
+                        type="text"
+                        {...register("licensePlate",{required: true})}
+                        value={formData.licensePlate}
+                        onChange={handleChange}  
+                    />
                     <div>
-                        <div>
-                            <p>ทะเบียนรถ: {val.licensePlate}</p>
-                            <p>ยี่ห้อ: {val.brand}</p>
-                            <p>รุ่น: {val.model}</p>
-                            <p>หมายเหตุ: {val.note}</p>
-                            <button onClick={()=> handleEdit(val)}>Edit</button>
-                            <button onClick={() => deleteCar(val.id)}>Delete</button>
-                        </div>
+                        {errors.licensePlate && <span className="text-red-500 text-sm">จำเป็นต้องกรอกทะเบียนรถ</span>}
                     </div>
-                )
-            })}
+                </div>
+                <div className='mb-2'>
+                    <input
+                        className="border-2 rounded-md p-2 w-[200px]"
+                        name='brand'
+                        placeholder='ยี่ห้อรถ' 
+                        type="text"
+                        {...register("brand",{required: true})}
+                        value={formData.brand}
+                        onChange={handleChange}  
+                    />
+                    <div>
+                        {errors.brand && <span className="text-red-500 text-sm">จำเป็นต้องกรอกยี่ห้อของรถ</span>}
+                    </div>
+                </div>
+                <div className='mb-2'>
+                    <input
+                        className="border-2 rounded-md p-2 w-[200px]"
+                        name='model'
+                        placeholder='รุ่นของรถ' 
+                        type="text"
+                        {...register("model",{required: true})}
+                        value={formData.model}
+                        onChange={handleChange}  
+                    />
+                    <div>
+                        {errors.model && <span className="text-red-500 text-sm">จำเป็นต้องกรอกรุ่นของรถ</span>}
+                    </div>
+                </div>
+                <div className='mb-2'>
+                    <input
+                        className="border-2 rounded-md p-2 w-[200px]"
+                        name='note'
+                        placeholder='หมายเหตุ' 
+                        type="text"
+                        {...register("note")}
+                        value={formData.note}
+                        onChange={handleChange} 
+                    />
+                </div>
+                <div className='flex justify-around'>
+                    <div className='mt-4'>
+                        <button type='submit' className="bg-black shadow-md text-white rounded-md px-4 py-2">{editCar ? 'Update' : 'ยืนยัน'}</button>
+                    </div>
+                    {editCar && <button type="button" className="bg-black shadow-md text-white rounded-md px-4 py-2 mt-4" onClick={resetForm}>Cancel</button>}
+                </div>
+            </form>
+        </div>    
+        <div className='mt-20'>
+            <button onClick={fetchData} className="bg-black shadow-md text-white rounded-md px-4 py-2 mb-4" >{showData ? 'Hide all cars' : 'Show all cars'}</button>
+            <div className='grid grid-cols-4 gap-4'>
+                {showData &&
+                carsList.map((val,key)=>{
+                    return (
+                        <div>
+                            <div className="border-2 rounded-md p-4">
+                                <p>ทะเบียนรถ: {val.licensePlate}</p>
+                                <p>ยี่ห้อ: {val.brand}</p>
+                                <p>รุ่น: {val.model}</p>
+                                <p>หมายเหตุ: {val.note}</p>
+                                <button onClick={()=> handleEdit(val)} className="bg-green-500 text-white rounded-md px-3 py-1 mr-2">Edit</button>
+                                <button onClick={() => deleteCar(val.id)} className="bg-red-500 text-white rounded-md px-3 py-1">Delete</button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     </div>    
   )
